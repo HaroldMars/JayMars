47e74df4409ab01565e88c62b3c6dfb92973343e
HaroldMars <abejar199@gmailcom>
2025-10-03 19:46:12 +0800

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="homie">
+      <div>
+        <h1>Hello! Im Jay Haorld Mars V. Abejar</h1>
+        <p>Student of UCMAIN.</p>
+      </div>
+    </div>
+  );
+}
