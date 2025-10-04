ba18d4f11b95a6b1f75ae97b40e7bb96735cd885
HaroldMars <abejar199@gmailcom>
2025-10-04 09:28:51 +0800

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import React from "react";
 export default function Home() {
   return (
     <div className="homie">
-      <div>
-        <h1>Hello! Im Jay Haorld Mars V. Abejar</h1>
-        <p>Student of UCMAIN.</p>
+      <div className="text font-bold">
+        <h1>
+  Hello! I'm <span className="underline underline-offset-2">Jay Harold Mars V. Abejar</span>
+</h1>
+        <p >Student of UCMAIN.</p>
       </div>
     </div>
   );
